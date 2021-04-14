309ba3dc2a46c3d35e70898ab33286baa4ccf2b7
Falence Lemungoh <falencelemungoh@gmail.com>
2021-04-14 23:40:04 +0100

    Create: Insert multiple documents

--- crud.js
+++ crud.js
@@ -14,6 +14,31 @@ async function main() {
             bathrooms: 1
         })
 
+        await createMultipleListings(client,[
+            {
+                name: "Infinite Views",
+                summary: "Modern home with infinite views from the infinity pool",
+                property_type: "House",
+                bedrooms: 5,
+                bathrooms: 4.5,
+                beds: 5
+            },
+            {
+                name: "Private room in London",
+                property_type: "Apartment",
+                bedrooms: 1,
+                bathroom: 1
+            },
+            {
+                name: "Beautiful Beach House",
+                summary: "Enjoy relaxed beach living in this house with a private beach",
+                bedrooms: 4,
+                bathrooms: 2.5,
+                beds: 7,
+                last_review: new Date()
+            }
+        ])
+
     } catch (e) {
         console.log(e)
     } finally {
@@ -28,6 +53,14 @@ async function createListing(client, newListing) {
         .collection('listingsAndReviews')
         .insertOne(newListing)
     console.log(`New listing created with the  following id: ${result.insertedId}`)
+}
+
+async function createMultipleListings(client, newListings) {
+    const result = await client.db('sample_airbnb')
+        .collection('listingsAndReviews')
+        .insertMany(newListings)
+    console.log(`${result.insertedCount} new listing(s) created with the following id(s): `)
+    console.log(result.insertedIds)
 }
 
 
