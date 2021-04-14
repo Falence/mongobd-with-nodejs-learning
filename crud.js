d06b6edb145381c9cc77b6d341db14e854360c0c
Falence Lemungoh <falencelemungoh@gmail.com>
2021-04-15 00:53:34 +0100

    Read: Find multiple documents by query

--- crud.js
+++ crud.js
@@ -7,7 +7,11 @@ async function main() {
     try {
         await client.connect() //connect to mongodb cluster
 
-        await readOneListingByName(client, 'Lovely Software Program')
+        await findListingsWithMinimumBedroomsAndMostRecentReviews(client, {
+            minimumNumberOfBedrooms: 4,
+            minimumNumberOfBathrooms: 2,
+            maximumNumberOfResults: 5
+        })
 
     } catch (e) {
         console.log(e)
@@ -48,6 +52,34 @@ async function readOneListingByName(client, nameOfListing) {
     }
 }
 
+// read multiple listings based on query
+async function findListingsWithMinimumBedroomsAndMostRecentReviews(client,{
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER
+} = {}){
+    const cursor = client.db('sample_airbnb').collection('listingsAndReviews')
+        .find({
+            bedrooms: {$gte: minimumNumberOfBedrooms},
+            bathrooms: {$gte: minimumNumberOfBathrooms}
+        }).sort({last_review: -1}).limit(maximumNumberOfResults)
+
+    const results = await cursor.toArray()
+
+    if (results.length > 0) {
+        console.log(`Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`)
+        results.forEach((result, i) => {
+            const date = new Date(result.last_review).toDateString()
+
+            console.log()
+            console.log(`${i + 1}, name: ${result.name}`)
+            console.log(`   _id: ${result._id}`)
+            console.log(`   bedrooms: ${result.bedrooms}`)
+            console.log(`   bathrooms: ${result.bathrooms}`)
+            console.log(`   most recent review date: ${new Date(result.last_review).toDateString()}`)
+        })
+    }
+}
 
 
 
